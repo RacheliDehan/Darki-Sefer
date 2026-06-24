1a20169e3fddf7532bb92e218bf909ba1d73b75c
רחל כהן <RachelC@bt.Local>
2026-06-24 12:14:32 +0300

    change the style

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ROUTES } from '../../core/constants/routes.constants';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
   public readonly year = new Date().getFullYear();
+    public readonly routes = ROUTES;
 }
